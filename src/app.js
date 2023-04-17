4a70147c5c0801291f6239740c3f2ec10b393748
Austin Willoughby <arwigm@rit.edu>
2023-04-17 15:20:42 -0400

    Updates to app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
+const RedisStore = require('connect-redis').default;
 const redis = require('redis');
 
+const router = require('./router.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/ConfigExample';
 
 mongoose.connect(dbURL).catch(err => {
@@ -20,51 +22,43 @@ mongoose.connect(dbURL).catch(err => {
   }
 });
 
-const redisURL = process.env.REDISCLOUD_URL || 
-  'REPLACE_WITH_YOUR_REDISCLOUD_URL';
-
 const redisClient = redis.createClient({
-  legacyMode: true,
-  url: redisURL,
+  url: process.env.REDISCLOUD_URL,
 });
-redisClient.connect().catch(console.error);
+redisClient.on('error', err => console.log('Redis Client Error', err));
 
+redisClient.connect().then(() => {
+  const app = express();
+  app.use('/assets', express.static(path.resolve(`${__dirname}../../client/`)));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({
+    extended: true,
+  }));
 
-// pull in our routes
-const router = require('./router.js');
+  app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Secret Session Key',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+    },
+  }));
 
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+  app.use(favicon(path.resolve(`${__dirname}/../client/img/favicon.png`)));
 
-const app = express();
-app.use('/assets', express.static(path.resolve(`${__dirname}../../client/`)));
-app.use(compression());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+  router(app);
 
-app.use(session({
-  key: 'sessionid',
-  store: new RedisStore({
-    client: redisClient,
-  }),
-  secret: 'Secret Session Key',
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-  },
-}));
-
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
-app.use(favicon(path.resolve(`${__dirname}/../client/img/favicon.png`)));
-
-router(app);
-
-app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+  app.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Listening on port ${port}`);
+  });
 });
